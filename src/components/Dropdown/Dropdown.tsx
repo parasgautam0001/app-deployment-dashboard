c735d40e8843bea19d3bfd1d663fb56c95f61fb3
Paras <Paras@Akashs-MacBook-Air.local>
2024-05-07 02:59:47 +0530

    topbar and dropdown added

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import DropOpen from '../../assets/DropOpen.svg';
+import './Dropdown.css';
+import { DropdownProps } from '../types/types';
+
+const Dropdown: React.FC<DropdownProps> = ({ defaultName, options, setApp }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (index) => {
+    setApp && setApp(index);
+    setIsOpen(false);
+  }
+
+  return (
+    <div className="dropdown">
+      <div className="dropdown-toggle" onClick={toggleDropdown}>
+        <div>{defaultName}</div>
+        <img src={DropOpen} className={`${isOpen ? 'open-icon' : ''}`} />
+      </div>
+      <div className={`dropdown-menu ${isOpen ? 'open' : ''}`}>
+        {options.map((option, index) => {
+          return <div onClick={() => handleOptionClick(index)} className='option-name'>{option.name}</div>
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
---
+++ src/components/Topbar/Topbar.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import '../Topbar/Topbar.css';
+import Dropdown from '../Dropdown/Dropdown.tsx';
+import { TopbarProps } from '../types/types.ts';
+
+const Topbar: React.FC<TopbarProps> = ({ application, appNames, setApp }) => {
+
+  return (
+    <div className='topbar-content'>
+      <div className='apps'>
+        <div className='applications'>Applications</div>
+        <Dropdown defaultName={application.name} options={appNames} setApp={(val) => setApp(val)} />
+      </div>
+      <div className='topbar-content'>
+        <div className='name-symbol'>PG</div>
+        <Dropdown defaultName="Paras Gautam" options={[{ id: 0, name: "Profile" }, { id: 1, name: "Logout" }]} />
+      </div>
+    </div>
+  );
+}
+
+export default Topbar;
