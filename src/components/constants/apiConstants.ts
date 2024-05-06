cdd835325ab610826ea7cf50ed5d7239aed37cb6
Paras <Paras@Akashs-MacBook-Air.local>
2024-05-07 02:54:11 +0530

    constants file

---
+++ src/components/constants/apiConstants.ts
@@ -0,0 +1,7 @@
+export const GET_APPLICATIONS = "https://retoolapi.dev/71NNjB/applications";
+
+export const GET_MEMORY_UTILIZATION = "https://retoolapi.dev/ybFVVH/memoryutilization";
+
+export const GET_CPU_UTILIZATION = "https://retoolapi.dev/Ymxfa2/cpuutilization";
+
+export const GET_EVENT_HISTORY = "https://retoolapi.dev/TYjDIe/eventhistory";
---
+++ src/components/constants/stringConstants.ts
@@ -0,0 +1,19 @@
+export const OVERVIEW = "overview";
+
+export const ALERTS_COMING_SOON = "Alerts will be coming soon";
+
+export const EVENTS_COMING_SOON = "Events History will be coming soon";
+
+export const ENVIRONMENT_VARIABLES = "Environment Variables";
+
+export const ALERTS = "Alerts";
+
+export const EVENT_HISTORY = "Event history";
+
+export const ENVIRONMENT = "environment";
+
+export const ALERT = "alert";
+
+export const HISTORY = "history";
+
+export const OVERVIEW_SMALL = 'overview';
